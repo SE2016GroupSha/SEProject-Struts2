90b21a96a8c68b182a31ec852239862031428672
luxin-xyz <luxin.xyz@qq.com>
2016-10-30 03:41:21 +0800

    Change fuzzy key define

--- src/xyz/sesha/project/store/index/UserIdKeysToDataIds.java
+++ src/xyz/sesha/project/store/index/UserIdKeysToDataIds.java
@@ -5,8 +5,8 @@ import org.apache.log4j.Logger;
 /**
  * 后端数据请求功能类：数据索引功能类
  * 
- * <p>形式1：user:[id]:search:data:fuzzy:all              ->(key1, key2...)
- * <br>形式2：user:[id]:search:data:fuzzy:[key]:datas     ->(id1, id2...)
+ * <p>形式1：user:[id]:search:data:fuzzy:all         ->(key1, key2...)
+ * <br>形式2：user:[id]:search:data:fuzzy:[key]:     ->(id1, id2...)
  * <br>说明：通过user的id和关键字,获取全部存在关键字的data的id，一对多映射
  * 
  * @author Administrator
